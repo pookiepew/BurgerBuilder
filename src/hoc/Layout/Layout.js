f0eae2d1029b3281c7a1207ad5f9b54bd95d7509
pookiepew <glensorbo@gmail.com>
2019-07-24 17:33:48 +0200

    Moved some folders

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react'
 
-import Aux from '../../hoc/Auxi'
+import Aux from '../Auxi/Auxi'
 import classes from './Layout.css'
-import Toolbar from '../Navigation/Toolbar/Toolbar'
-import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar'
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer'
 
 class Layout extends Component {
 
   state = {
-    showSideDrawer: true
+    showSideDrawer: false
   }
 
   sideDrawerClosedHandler = () => {
